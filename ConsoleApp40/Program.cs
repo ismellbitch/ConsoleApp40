69dda4cd724c76ff28e78c1a822d2aa1e70f99d0
user10 <work20go@mail.ru>
2022-12-19 11:53:35 +0200

    Add project files.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp39;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Введите y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        Solution solution = new(x, y);
+
+        if (x >= -2 && x < 0) Console.WriteLine(solution.GetRightSideSolution());
+        else if (x <= 2 && x > 0) Console.WriteLine(solution.GetRightSideSolution());
+        else Console.WriteLine(solution.GetZeroSolution());
+    }
+}
---
+++ ConsoleApp40/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    internal class Solution
+    {
+        private double x;
+        private double y;
+
+        public Solution(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public string GetLeftSideSolution()
+        {
+            if (x >= -2 && x < 0 && y >= -2 + Math.Abs(x) && y <= 2 && y >= 0) return "Попадает";
+            else return "Не попадает";
+        }
+        public string GetRightSideSolution()
+        {
+            if (x <= 2 && x > 0 && y >= 2 - Math.Abs(x) && y <= 2 && y >= 0) return "Попадает";
+            else return "Не попадает";
+        }
+        public string GetZeroSolution()
+        {
+            if (x == 0 && y == 2) return "Попадает";
+            else return "Не попадает";
+        }
+    }
+}
